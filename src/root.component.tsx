698ddd57fc8cc0b251c79aa7891f818726d1230d
Daniel Anbarestani <Daniel.Anbarestani@iCloud.com>
2020-08-23 21:25:20 +0200

    Implement eventmanager and change styling

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { InputGroup, FormControl, Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { Card, Alert, Container, Row, Col } from "react-bootstrap";
+import { Priority, Todo, TodoSubject } from "@dananb/eventmanager";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Root(props) {
-  const todos = ["Learn Javascript", "Learn AWS", "Become a mage"];
+  const [highPriority, setHighPriority] = useState<Todo[]>([]);
+  const [mediumPriority, setMediumPriority] = useState<Todo[]>([]);
+  const [lowPriority, setLowPriority] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const subscription = TodoSubject.subscribe((newTodo: Todo) => {
+      switch (newTodo.priority) {
+        case Priority.HIGH:
+          setHighPriority((existingTodos) => [...existingTodos, newTodo]);
+          break;
+        case Priority.MEDIUM:
+          setMediumPriority((existingTodos) => [...existingTodos, newTodo]);
+          break;
+        case Priority.LOW:
+          setLowPriority((existingTodos) => [...existingTodos, newTodo]);
+          break;
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
     <Container fluid>
@@ -16,18 +38,45 @@ export default function Root(props) {
       >
         <Col xs="auto">
           <h1>({props.name})</h1>
-          {todos.map((todo) => (
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-              </InputGroup.Prepend>
-              <FormControl
-                disabled
-                aria-label="Text input with checkbox"
-                value={todo}
-              />
-            </InputGroup>
-          ))}
+          <Row style={{ marginBottom: "2rem" }}>
+            <Col xs="12">
+              <Card>
+                <Card.Header>High priority</Card.Header>
+                <Card.Body id="high-priority">
+                  {highPriority.map((todo) => (
+                    <Alert variant={"danger"}>{todo.description}</Alert>
+                  ))}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: "2rem" }}>
+            <Col xs="12">
+              <Card>
+                <Card.Header>Medium priority</Card.Header>
+                <Card.Body id="medium-priority">
+                  {mediumPriority.map((todo) => (
+                    <Alert variant={"warning"}>{todo.description}</Alert>
+                  ))}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: "2rem" }}>
+            <Col xs="12">
+              <Card>
+                <Card.Header>Low priority</Card.Header>
+                <Card.Body id="low-priority">
+                  {lowPriority.map((todo) => (
+                    <Alert variant={"success"}>{todo.description}</Alert>
+                  ))}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col id="low-priority" xs="12"></Col>
+          </Row>
         </Col>
       </Row>
     </Container>
